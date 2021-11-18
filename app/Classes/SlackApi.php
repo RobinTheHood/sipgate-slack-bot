6865a6fc896a1c70473690bca57143a25c334a9b
Robin Wieschendorf <mail@robinwieschendorf.de>
2021-11-18 17:56:19 +0100

    feat: use formatted slack message

--- app/Classes/SlackApi.php
+++ app/Classes/SlackApi.php
@@ -17,7 +17,7 @@ class SlackApi
         $this->client = new Client(['base_uri' => 'https://slack.com/api/']);
     }
 
-    private function sendRequest(string $url, string $method = 'GET', array $options = []): ?array
+    private function sendRequest(string $url, string $method = 'GET', array $options = []): void
     {
         $response = $this->client->request($method, $url, array_merge(
             [
@@ -28,88 +28,16 @@ class SlackApi
             ],
             $options
         ));
-
-        var_dump($response);
-        die('test2');
     }
 
-    public function sentMessage()
+    public function sentMessage(string $channel, array $message): void
     {
         $options = [
             'json' => [
-                'channel' => "#sipgate",
-                'text' => "I hope the tour went well, Mr. Wonka."
+                'channel' => $channel,
+                'blocks' => $message
             ]
         ];
-
-        $block = [
-            [
-                'type' => 'section',
-                'text' => [
-                    'type' => 'mrkdwn',
-                    'text' => 'Neue Benachrichtigung von sipgate',
-                ],
-            ],
-            [
-                'type' => 'section',
-                'fields' => [
-                    [
-                        'type' => 'mrkdwn',
-                        'text' => "*Von:*\n+491234567",
-                    ],
-                    [
-                        'type' => 'mrkdwn',
-                        'text' => "*Von Name:*\nMax Mustermann",
-                    ],
-                    [
-                        'type' => 'mrkdwn',
-                        'text' => "*An:*\n+497654321",
-                    ],
-                    [
-                        'type' => 'mrkdwn',
-                        'text' => "*An Name:*\nZentrale",
-                    ],
-                    [
-                        'type' => 'mrkdwn',
-                        'text' => "*Wann:*\n18:40:21 - 17.11.2021",
-                    ],
-                ],
-            ],
-            [
-                'type' => 'actions',
-                'elements' => [
-                    [
-                        'type' => 'button',
-                        'text' => [
-                            'type' => 'plain_text',
-                            'emoji' => true,
-                            'text' => 'Rückruf',
-                        ],
-                        'style' => 'primary',
-                        'value' => 'click_me_123',
-                    ],
-                    [
-                        'type' => 'button',
-                        'text' => [
-                            'type' => 'plain_text',
-                            'emoji' => true,
-                            'text' => 'Ansehen',
-                        ],
-                        'url' => 'https://app.sipgate.com/history'
-                    ],
-                ]
-            ]
-        ];
-
-        $options = [
-            'json' => [
-                'channel' => "#sipgate",
-                //'text' => $block
-                'blocks' => $block
-            ]
-        ];
-
-        // {"channel":"C061EG9SL","text":"I hope the tour went well, Mr. Wonka.","attachments":[{"text":"Who wins the lifetime supply of chocolate?","fallback":"You could be telling the computer exactly what it can do with a lifetime supply of chocolate.","color":"#3AA3E3","attachment_type":"default","callback_id":"select_simple_1234","actions":[{"name":"winners_list","text":"Who should win?","type":"select","data_source":"users"}]}]}
 
         $this->sendRequest('chat.postMessage', 'POST', $options);
     }
