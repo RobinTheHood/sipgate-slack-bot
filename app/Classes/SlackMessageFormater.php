0de82d8ec22f46fd7c1a475ac2e6f688d6a636eb
Robin Wieschendorf <mail@robinwieschendorf.de>
2021-11-18 17:55:21 +0100

    feat: format slack message with history data

--- app/Classes/SlackMessageFormater.php
+++ app/Classes/SlackMessageFormater.php
@@ -6,6 +6,41 @@ class SlackMessageFormater
 {
     public function formatHistoryEntry(HistoryEntry $historyEntry)
     {
+        $actionElements = [];
+
+        if ($historyEntry->getRecordingUrl()) {
+            $actionElements[] = [
+                'type' => 'button',
+                'text' => [
+                    'type' => 'plain_text',
+                    'emoji' => true,
+                    'text' => 'Nachricht abhören',
+                ],
+                'url' => $historyEntry->getRecordingUrl()
+            ];
+        }
+
+        $actionElements[] = [
+            'type' => 'button',
+            'text' => [
+                'type' => 'plain_text',
+                'emoji' => true,
+                'text' => 'Rückruf',
+            ],
+            'style' => 'primary',
+            'value' => 'click_me_123',
+        ];
+
+        $actionElements[] = [
+            'type' => 'button',
+            'text' => [
+                'type' => 'plain_text',
+                'emoji' => true,
+                'text' => 'Ansehen',
+            ],
+            'url' => 'https://app.sipgate.com/history'
+        ];
+
         $block = [
             [
                 'type' => 'section',
@@ -19,49 +54,33 @@ class SlackMessageFormater
                 'fields' => [
                     [
                         'type' => 'mrkdwn',
-                        'text' => "*Von:*\n+491234567",
+                        'text' => "*Von:*\n" . $historyEntry->getSource(),
                     ],
                     [
                         'type' => 'mrkdwn',
-                        'text' => "*Von Name:*\nMax Mustermann",
+                        'text' => "*Von Name:*\n" . $historyEntry->getSourceAlias(),
                     ],
                     [
                         'type' => 'mrkdwn',
-                        'text' => "*An:*\n+497654321",
+                        'text' => "*An:*\n" . $historyEntry->getTarget(),
                     ],
                     [
                         'type' => 'mrkdwn',
-                        'text' => "*An Name:*\nZentrale",
+                        'text' => "*An Name:*\n" . $historyEntry->getTargetAlias(),
                     ],
                     [
                         'type' => 'mrkdwn',
-                        'text' => "*Wann:*\n18:40:21 - 17.11.2021",
+                        'text' => "*Wann:*\n" . $historyEntry->getCreated()->format('d.m.Y H:i'),
+                    ],
+                    [
+                        'type' => 'mrkdwn',
+                        'text' => "*Angenommen von:*\n" . $historyEntry->getResponderAlias(),
                     ],
                 ],
             ],
             [
                 'type' => 'actions',
-                'elements' => [
-                    [
-                        'type' => 'button',
-                        'text' => [
-                            'type' => 'plain_text',
-                            'emoji' => true,
-                            'text' => 'Rückruf',
-                        ],
-                        'style' => 'primary',
-                        'value' => 'click_me_123',
-                    ],
-                    [
-                        'type' => 'button',
-                        'text' => [
-                            'type' => 'plain_text',
-                            'emoji' => true,
-                            'text' => 'Ansehen',
-                        ],
-                        'url' => 'https://app.sipgate.com/history'
-                    ],
-                ]
+                'elements' => $actionElements
             ]
         ];
 
