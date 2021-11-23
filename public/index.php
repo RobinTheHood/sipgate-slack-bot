44d2916357ee88ef1dd6da4d69d80bea009d3811
Robin Wieschendorf <mail@robinwieschendorf.de>
2021-11-23 11:36:13 +0100

    chore: new index.php with link to github

--- public/index.php
+++ public/index.php
@@ -1,46 +1,18 @@
-<?php
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-restore_error_handler();
-restore_exception_handler();
-
-require_once '../vendor/autoload.php';
-
-function testCron()
-{
-    $cronjobController = new App\Classes\Controller\CronjobController();
-    $cronjobController->runOnce();
-}
-
-function testSipgateApi()
-{
-    $sipgateApi = new App\Classes\Sipgate\SipgateApi(
-        App\Config\Config::SIPGATE_API_USERNAME,
-        App\Config\Config::SIPGATE_API_PASSWORD
-    );
-
-    //$sipgateApi->call();
-    $sipgateApi->getAllHistoryEntries();
-}
-
-function testSlackApi()
-{
-    $slackApi = new App\Classes\Slack\SlackApi(
-        App\Config\Config::SLACK_API_TOKEN
-    );
-
-    //$slackApi->sentMessage();
-}
-
-function testApiController()
-{
-    $apiController = new App\Classes\Controller\ApiController();
-    $apiController->invoke();
-}
-
-testApiController();
-//testCron();
-//testSipgateApi();
-//testSlackApi();
+<hmtl>
+    <head>
+        <title>Sipgatge Slack Bot</title>
+        <style>
+            body {
+                margin-left: auto;
+                margin-right: auto;
+                max-width: 800px;
+                margin-top: 50px;
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>🤖 Hello, I'm the Sipgate Slack Bot</h1>
+        <p>You can get more infos at: <a target="_blank" href="https://github.com/RobinTheHood/sipgate-slack-bot">github.com/RobinTheHood/sipgate-slack-bot</a>
+    </body>
+</hmtl>
